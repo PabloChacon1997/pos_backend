605f8cc1c6af6024bb34da044edf82b26cd92102
Pablo Andres Chacon <pabloandres120297@gmail.com>
2026-04-15 19:43:12 -0500

    add pagination products, update product and delete, finshed CRUD products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { Repository } from 'typeorm';
+import { FindManyOptions, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { Product } from './entities/product.entity';
@@ -32,30 +32,69 @@ export class ProductsService {
     });
   }
 
-  async findAll() {
-    const [data, total] = await this.productRepository.findAndCount({
+  async findAll(categoryId: number | null, take: number, skip: number) {
+    const options: FindManyOptions<Product> = {
       relations: {
         category: true,
       },
       order: {
         id: 'ASC',
       },
-    });
+      take,
+      skip,
+    };
+    if (categoryId) {
+      options.where = {
+        category: {
+          id: categoryId,
+        },
+      };
+    }
+    const [prodycts, total] =
+      await this.productRepository.findAndCount(options);
     return {
-      data,
+      prodycts,
       total,
     };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.productRepository.findOne({
+      where: {
+        id,
+      },
+      relations: {
+        category: true,
+      },
+    });
+    if (!product) {
+      const errors: string[] = [];
+      errors.push(`El producto no fue encontrado con el ID: ${id}`);
+      throw new NotFoundException(errors);
+    }
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const product = await this.findOne(id);
+    Object.assign(product, updateProductDto);
+    if (updateProductDto.categoryId) {
+      const category = await this.categoryRepository.findOneBy({
+        id: updateProductDto.categoryId,
+      });
+      if (!category) {
+        const errors: string[] = [];
+        errors.push('La categoria no existe');
+        throw new NotFoundException(errors);
+      }
+      product.category = category;
+    }
+    return await this.productRepository.save(product);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    const product = await this.findOne(id);
+    await this.productRepository.remove(product);
+    return 'Producto eliminado correctamente';
   }
 }
