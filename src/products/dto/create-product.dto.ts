3d6422579d8ef8c5f2e18459b509cdd083d4e4ad
Pablo Andres Chacon <pabloandres120297@gmail.com>
2026-05-06 18:11:59 -0500

    upload image with product

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -5,6 +5,9 @@ export class CreateProductDto {
   @IsString({ message: 'Nombre no válido' })
   name: string;
 
+  @IsNotEmpty({ message: 'Imagen del producto es obligatorio' })
+  image: string;
+
   @IsNotEmpty({ message: 'Precio del producto es obligatorio' })
   @IsNumber({ maxDecimalPlaces: 2 }, { message: 'Precio no válido' })
   price: number;
